c7dee085f70377c4eccf84eb1cf778de7559489d
Michael Dijkstra <micdijkstra@gmail.com>
2015-09-25 15:52:14 +1000

    Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,21 @@
-var _ = require('lodash'),
-    gulp = require('gulp');
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    vfs = require('vinyl-fs'),
-    concat = require('gulp-concat'),
-    install = require("gulp-install");
+var gulp = require('gulp');
 
-gulp.task('build', function() {
+gulp.task('default', function() {
   var assets = {
     javascripts: [
-      './bower_components/jquery/dist/jquery.js',
-      './bower_components/modernizr/modernizr.js',
-      './bower_components/respond.js/dest/respond.min.js',
-      './bower_components/bootstrap-sass/assets/javascripts/**/*',
+      './node_modules/jquery/dist/jquery.js',
+      './node_modules/modernizr/modernizr.js',
+      './node_modules/respond.js/dest/respond.min.js',
+      './node_modules/bootstrap-sass/assets/javascripts/*',
+      './node_modules/bootstrap-sass/assets/javascripts/*/**',
     ],
     stylesheets: [
-      './bower_components/bootstrap-sass/assets/stylesheets/**/*'
+      './node_modules/bootstrap-sass/assets/stylesheets/*',
+      './node_modules/bootstrap-sass/assets/stylesheets/*/**',
     ]
   };
 
   for (var type in assets) {
-    gulp.src(assets[type]).pipe(gulp.dest('./build/' + type + '/lib'));
+    gulp.src(assets[type]).pipe(gulp.dest('assets/' + type + '/lib'));
   };
-
-  gulp.src('./stylesheets/**/*').pipe(gulp.dest('./build/stylesheets'));
 });
-
-
-gulp.task('styles', function() {
-  return sass('./build/stylesheets/stencil.scss', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(gulp.dest('./build/stylesheets'));
-});
-
-gulp.task('scripts', function() {
-  return gulp.src('./javascripts/*')
-  .pipe(concat('stencil.js'))
-  .pipe(gulp.dest('./build/javascripts/'))
-});
-
-gulp.task('default', ['build', 'styles', 'scripts'], function() {});
